c288c685db31ebffaebdd63406150e455acbf991
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-18 23:07:27 +0200

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-// Component igenzura ko umuntu afie uurenganz runaka
+// Component igenzura ko umuntu afie uurengaz runaka
 const ProtectedRoute = ({ requiredRole }) => {
   const { userRole, isLoading } = useAuth();
 
@@ -20,6 +20,7 @@ const ProtectedRoute = ({ requiredRole }) => {
 };
 
 export default ProtectedRoute;
+
 
 
 
